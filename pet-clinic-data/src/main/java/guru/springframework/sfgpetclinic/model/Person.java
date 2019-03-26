9d3068d636860cfda8450faea05e5911afa9eb99
sambitc <sambit04126@gmail.com>
2019-03-26 05:30:33 +0530

    BaseEntity created, id added. Closes #10.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -3,7 +3,8 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Data;
 
 @Data
-public class Person {
+public class Person extends BaseEntity{
+
     private String firstName;
     private String lastName;
 }
