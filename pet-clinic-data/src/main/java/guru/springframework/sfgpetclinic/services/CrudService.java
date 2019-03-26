9e160dabaccca43a1be71406e3629cb157295377
sambitc <sambit04126@gmail.com>
2019-03-26 07:18:10 +0530

    Refactored interfaces. Closes #11.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T,ID> {
+    Set<T> findAll();
+    T save(T t);
+    T findById(ID id);
+    void delete(T t);
+    void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -3,12 +3,8 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Owner findById(long id);
+public interface OwnerService extends CrudService<Owner,Long>{
     Owner findByLastName(String lastName);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 
 }
